637c9d1e6f43138cbbdbc792cfe3d8283d10671d
Rodney <hardworker30@gmail.com>
2020-03-04 19:31:58 -0500

    updated script

--- aws-inspec/scripts/aws_list.sh
+++ aws-inspec/scripts/aws_list.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
+set -x
+dir="/tmp/"
+echo "${dir}"
 
-aws-list-all query --region us-east-2 --service ec2 --directory ./tmp/
+aws-list-all query --region us-east-2 --directory "${dir}"
+
+aws s3 cp ${dir} s3://inspec-data
+
+echo "copied data to s3 bucket"
+
